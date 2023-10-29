6bf949cc91443ebb38c53ed8362e38b43e2c5e3a
Hassan <islamicrule999@gmail.com>
2023-10-29 22:37:32 +0500

    Real programe for bgCOlorCHanger

--- bgColorChanger/src/App.jsx
+++ bgColorChanger/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("black");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen duration-700"
+        style={{ background: color }}
+      >
+        <div className="flex flex-wrap justify-center fixed bottom-12 inset-x-0 ">
+          <div className="flex flex-wrap justify-center outline-none bg-white px-4 py-3 rounded-lg gap-4">
+            <button
+              onClick={() => setColor("yellow")}
+              className=" outline-none bg-yellow-700 px-4 py-2 rounded-lg"
+            >
+              yellow
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className=" outline-none bg-blue-700 px-4 py-2 rounded-lg"
+            >
+              blue
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className=" outline-none bg-green-700 px-4 py-2 rounded-lg"
+            >
+              green
+            </button>
+            <button
+              onClick={() => setColor("pink")}
+              className=" outline-none bg-pink-400  px-4 py-2 rounded-lg"
+            >
+              pink
+            </button>
+            <button
+              onClick={() => setColor("orange")}
+              className=" outline-none bg-orange-700 px-4 py-2 rounded-lg"
+            >
+              orange
+            </button>
+            <button
+              onClick={() => setColor("purple")}
+              className=" outline-none bg-purple-700 px-4 py-2 rounded-lg"
+            >
+              purple
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
